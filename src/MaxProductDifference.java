509bc755fdee163339095a0c5c48c1eccaed2256
Janaki Narayanasamy <janakijanaki667@gmail.com>
2023-12-18 23:16:18 +0530

    added files

---
+++ src/MaxProductDifference.java
@@ -0,0 +1,24 @@
+public class MaxProductDifference {
+    public static void main(String[] args) {
+    int[] arr = {4,2,5,9,7,4,8};
+        System.out.println(maxProductDifference(arr));
+
+    }
+    public static int maxProductDifference(int[] nums) {
+        int max=Integer.MIN_VALUE, max1=Integer.MIN_VALUE, min=Integer.MAX_VALUE, min1=Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num >= max) {
+                max1 = max;
+                max = num;
+            } else if (num > max1)
+                max1 = num;
+
+            if (num <= min) {
+                min1 = min;
+                min = num;
+            } else if (num < min1)
+                min1 = num;
+        }
+        return (max * max1) - (min * min1);
+    }
+}
