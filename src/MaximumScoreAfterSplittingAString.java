86cb9f6f9cf0de361c9774bc367f832ef7d85d70
Janaki Narayanasamy <janakijanaki667@gmail.com>
2023-12-22 20:31:58 +0530

    added files

---
+++ src/MaximumScoreAfterSplittingAString.java
@@ -0,0 +1,22 @@
+public class MaximumScoreAfterSplittingAString {
+    public static void main(String[] args) {
+        String s = "00111";
+        System.out.println(maxScore(s));
+    }
+
+    public static int maxScore(String s) {
+            int maxScore = 0;
+            int countZerosLeft = 0;
+            int countOnesRight = (int) s.chars().filter(ch -> ch == '1').count();
+
+
+            for (int i = 0; i < s.length() - 1; i++) {
+                countZerosLeft += s.charAt(i) == '0' ? 1 : 0;
+                countOnesRight -= s.charAt(i) == '1' ? 1 : 0;
+                maxScore = Math.max(maxScore, countZerosLeft + countOnesRight);
+            }
+
+            return maxScore;
+        }
+    }
+
