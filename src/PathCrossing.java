25cd90abf9872a2b3794cb27519578913d1092de
Janaki Narayanasamy <janakijanaki667@gmail.com>
2023-12-23 12:38:31 +0530

    added files

---
+++ src/PathCrossing.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+
+public class PathCrossing {
+    public static void main(String[] args) {
+        String path = "NESWW";
+        System.out.println(isPathCrossing(path));
+    }
+    public static boolean isPathCrossing(String path) {
+        int count=0;
+        int count1=0;
+        HashSet<String> points = new HashSet<>();
+        points.add("0_0");
+        for (int i =0; i< path.length();i++){
+            switch (path.charAt(i)){
+                case 'N' : count1++; break;
+                case 'S' : count1--; break;
+                case 'W' : count--; break;
+                case 'E' : count++; break;
+            }
+
+            //string builder performs faster than string ,because string has to create new objects in heap memory while concatenating
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(count);
+            stringBuilder.append("-");
+            stringBuilder.append(count1);
+            String s= stringBuilder.toString();
+            if(!points.add(s)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
