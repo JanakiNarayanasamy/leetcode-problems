ee2bc11044c7a16c52ffc6104f13bd81f5a2fd22
Janaki Narayanasamy <janakijanaki667@gmail.com>
2024-01-01 22:30:48 +0530

    added file

---
+++ src/AssignCookies.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class AssignCookies {
+    public static void main(String[] args) {
+        int[] g = {1,2,3};
+        int[] s = {1,1};
+        System.out.println(findContentChildren(g,s));
+    }
+    public static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        for(int j=0;i<g.length && j<s.length;j++)
+            if(g[i]<=s[j]) i++;
+        return i;
+    }
+}
